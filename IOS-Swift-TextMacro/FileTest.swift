d28099fd9fe3801c747bde4147f761ce47b7fc48
Pooya Hatami <pooya@node-1w7jr9qrok5tfa7qt1xqikjoe.ipv6.telus.net>
2018-05-15 06:44:53 -0700

    done!

---
+++ IOS-Swift-TextMacro/FileTest.swift
@@ -0,0 +1,9 @@
+///*
+/*
+__PROJECTNAME__
+__FILENAME__
+Created by: __USERNAME__ on __DATE__
+Copyright (c) __YEAR__ My Company
+*/
+
+import Foundation
---
+++ IOS-Swift-TextMacro/Filetest01.swift
@@ -0,0 +1,9 @@
+///*
+/*
+IOS-Swift-TextMacro
+Filetest01.swift
+Created by: pooya on 2018-05-15
+Copyright (c) 2018 My Company
+*/
+
+import Foundation
